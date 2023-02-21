a5d9a64b5f509b3a09545e9923015a4e98f7098c
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-02-21 23:36:28 +0200

    add login page

---
+++ src/scripts/login.js
@@ -0,0 +1,28 @@
+
+let loginForm = document.getElementById('form')
+let warningWord = document.getElementById('wrongPass')
+warningWord.style.display = 'none';
+
+
+// check if data in localstorage is the same as user write or not
+
+loginForm.addEventListener('submit' , function(event){
+
+    let validEmail = localStorage.getItem('email')
+    let validPassword = localStorage.getItem('password')
+    let loginEmail = document.getElementById('floatingInput')
+    let loginPassword = document.getElementById('floatingPassword')
+
+    if(loginEmail.value === validEmail && loginPassword.value === validPassword){
+
+        loginForm.setAttribute('action', './nav.html' )
+        loginForm.setAttribute('method', 'GET' )
+        warningWord.style.display = 'none';
+
+    }else{
+        warningWord.style.display = 'block'
+        event.preventDefault()
+    }
+ 
+})
+

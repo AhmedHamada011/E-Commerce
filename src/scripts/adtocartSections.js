0d754d63926e411e2a28876b89cfe9af4f811ed2
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-02-26 16:38:01 +0200

    add-to-cart-from-sections

---
+++ src/scripts/adtocartSections.js
@@ -0,0 +1,22 @@
+
+
+// document.querySelectorAll(".product-addtocart-btn").forEach(btn=>{
+//     btn.addEventListener("click",function(e){
+//         // getProductId(e.EventTarget);
+//         console.log(this);
+//     })
+// })
+function addToCart(){
+    
+}
+
+
+function addToLocalSt(){
+
+
+}
+
+function getProductId(product){
+    
+    console.log(product.closest(".product-id").innerText)
+}
